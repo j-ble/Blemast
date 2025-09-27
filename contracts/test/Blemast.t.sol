a3fdf2f1a102e45ae2d7eeb41644bd34246d8ceb
j-ble <blemasterjacob@gmail.com>
2025-09-26 22:08:19 -0700

    100% coverage

--- contracts/test/Blemast.t.sol
+++ contracts/test/Blemast.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {Blemast} from "../src/Blemast.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 
 contract BlemastTest is Test {
     Blemast public blemast;
@@ -22,5 +23,29 @@ contract BlemastTest is Test {
     function testMint() public {
         blemast.mint(address(1), 100 ether);
         assertEq(blemast.balanceOf(address(1)), 100 ether);
+    }
+
+    function testPauseAndUnpause() public {
+        blemast.mint(address(this), 100 ether);
+        assertEq(blemast.balanceOf(address(this)), 100 ether);
+
+        blemast.pause();
+
+        vm.expectRevert(abi.encodeWithSelector(Pausable.EnforcedPause.selector));
+        blemast.transfer(address(2), 10 ether);
+
+        blemast.unpause();
+
+        blemast.transfer(address(2), 10 ether);
+        assertEq(blemast.balanceOf(address(2)), 10 ether);
+    }
+
+    function testPauseAndUnpauseFromNonOwner() public {
+        vm.startPrank(address(2));
+        vm.expectRevert();
+        blemast.pause();
+        vm.expectRevert();
+        blemast.unpause();
+        vm.stopPrank();
     }
 }
